a413a974fe2144bd14a832af333f902b9ca93d97
ken endo <ken223@gmail.com>
2013-03-11 22:54:24 +0900

    fix path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-rm -rf ../.vimrc
-rm -rf ../.vim
-ln -s .vimrc ../.vimrc
-ln -s .vim ../.vim
+if [ $1="delete" ] ; then
+  rm -rf ../.vimrc
+  rm -rf ../.vim
+fi
+ln -s ${HOME}/dotfiles/_vimrc ${HOME}/.vimrc
+ln -s ${HOME}/dotfiles/_vim ${HOME}/.vim
 
 #git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
