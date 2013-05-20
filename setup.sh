95e17ee7823ac9924543faa15714ab43b7af627a
endo ken <endouk@s1.docomo-tech.co.jp>
2013-05-20 12:57:10 +0900

    add log

--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@
 
 if [ $1="delete" ] ; then
   rm -rf ../.vimrc
+  echo "rm -rf ../.vimrc"
   rm -rf ../.vim
+  echo "rm -rf ../.vim"
 fi
 ln -s ${HOME}/dotfiles/_vimrc ${HOME}/.vimrc
+echo "create symboliclink for ../.vimrc"
 ln -s ${HOME}/dotfiles/_vim ${HOME}/.vim
+echo "create symboliclink for ../.vim"
 
